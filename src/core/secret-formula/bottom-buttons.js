62789efc519471686275486d34faeff13f9e9673
Blancas <106887785+Blancas7@users.noreply.github.com>
2026-02-22 17:39:30 +0100

    Version 0.1.6
    Added more bottom buttons in mobile
    Better handling of the time theorem box position in mobile

--- src/core/secret-formula/bottom-buttons.js
+++ src/core/secret-formula/bottom-buttons.js
@@ -7,10 +7,38 @@ export const bottomButtons = [
     condition: () => true,
   },
   {
+    key: "d",
+    name: "Dim Boost",
+    type: "bindRepeatableHotkey",
+    action: () => manualRequestDimensionBoost(true),
+    condition: () => true,
+  },
+  {
+    key: "g",
+    name: "Galaxy",
+    type: "bindRepeatableHotkey",
+    action: () => manualRequestGalaxyReset(true),
+    condition: () => true,
+  },
+  {
+    key: "c",
+    name: "Crunch",
+    type: "bindRepeatableHotkey",
+    action: () => manualBigCrunchResetRequest(),
+    condition: () => PlayerProgress.rewindUnlocked() || PlayerProgress.realityUnlocked() || PlayerProgress.eternityUnlocked() || PlayerProgress.infinityUnlocked(),
+  },
+  {
     key: "r",
     name: "R. Galaxy",
     type: "bindHotkey",
     action: () => replicantiGalaxyRequest(),
     condition: () => PlayerProgress.replicantiUnlocked() || PlayerProgress.rewindUnlocked(),
-  }
+  },
+  {
+    key: "e",
+    name: "Eternity",
+    type: "bindRepeatableHotkey",
+    action: () => eternityResetRequest(),
+    condition: () => PlayerProgress.rewindUnlocked() || PlayerProgress.eternityUnlocked() || Player.canEternity,
+  },
 ];
